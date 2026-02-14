98e030bfde26e70f70176deb221e00c4d44ed08f
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2026-02-14 14:34:34 +0530

    Initialize repository

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@heroui/react";
+
+export default function Home() {
+  return (
+    <div className="font-sans">
+      <Button variant="primary">Hello</Button>
+    </div>
+  );
+}
